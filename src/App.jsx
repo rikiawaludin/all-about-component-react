2d8ff225296dd3fb88e58e319c376edd17b0a2e9
Riki Awaludin <rikiawaludin17@gmail.com>
2024-06-03 13:40:28 +0700

    checkpoint pecah UI menjadi component2

--- src/App.jsx
+++ src/App.jsx
@@ -96,9 +96,6 @@ function NumResult() {
 function Main() {
   const [animes, setAnimes] = useState(animesData);
   const [selectedAnime, setSelectedAnime] = useState(animes[0]);
-  const [isOpen1, setIsOpen1] = useState(true);
-  const [isOpen2, setIsOpen2] = useState(true);
-
   function handleSelectedAnime(id) {
     const newAnime = animes.filter((anime) => anime.mal_id === id);
     setSelectedAnime(newAnime[0]);
@@ -106,61 +103,91 @@ function Main() {
 
   return (
     <main className="main">
-      <div className="box">
-        <button
-          className="btn-toggle"
-          onClick={() => setIsOpen1((open) => !open)}
-        >
-          {isOpen1 ? "–" : "+"}
-        </button>
-        {isOpen1 && (
-          <ul className="list list-anime">
-            {animes?.map((anime) => (
-              <li
-                key={anime.mal_id}
-                onClick={() => handleSelectedAnime(anime.mal_id)}
-              >
-                <img src={anime.image} alt={`${anime.title} cover`} />
-                <h3>{anime.title}</h3>
-                <div>
-                  <p>
-                    <span>{anime.year}</span>
-                  </p>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-      <div className="box">
-        <button
-          className="btn-toggle"
-          onClick={() => setIsOpen2((open) => !open)}
-        >
-          {isOpen2 ? "–" : "+"}
-        </button>
-        {isOpen2 && (
-          <div className="details">
-            <header>
-              <img
-                src={selectedAnime.image}
-                alt={`${selectedAnime.title} cover`}
-              />
-              <div className="details-overview">
-                <h2>{selectedAnime.title}</h2>
-                <p>
-                  {selectedAnime.year} &bull; {selectedAnime.score}
-                </p>
-              </div>
-            </header>
-            <section>
-              <p>
-                <em>{selectedAnime.synopsis}</em>
-              </p>
-            </section>
-          </div>
-        )}
-      </div>
+      <ListBox animes={animes} onSelectedAnime={handleSelectedAnime} />
+      <SelectedBox selectedAnime={selectedAnime} />
     </main>
+  );
+}
+
+function ListBox({ animes, onSelectedAnime }) {
+  const [isOpen1, setIsOpen1] = useState(true);
+
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen1((open) => !open)}
+      >
+        {isOpen1 ? "–" : "+"}
+      </button>
+      {isOpen1 && (
+        <AnimeList animes={animes} onSelectedAnime={onSelectedAnime} />
+      )}
+    </div>
+  );
+}
+
+function AnimeList({ animes, onSelectedAnime }) {
+  return (
+    <ul className="list list-anime">
+      {animes?.map((anime) => (
+        <Anime
+          key={anime.mal_id}
+          anime={anime}
+          onSelectedAnime={onSelectedAnime}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function Anime({ anime, onSelectedAnime }) {
+  return (
+    <li onClick={() => onSelectedAnime(anime.mal_id)}>
+      <img src={anime.image} alt={`${anime.title} cover`} />
+      <h3>{anime.title}</h3>
+      <div>
+        <p>
+          <span>{anime.year}</span>
+        </p>
+      </div>
+    </li>
+  );
+}
+
+function SelectedBox({ selectedAnime }) {
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  return (
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen2((open) => !open)}
+      >
+        {isOpen2 ? "–" : "+"}
+      </button>
+      {isOpen2 && <AnimeDetail selectedAnime={selectedAnime} />}
+    </div>
+  );
+}
+
+function AnimeDetail({selectedAnime}) {
+  return (
+    <div className="details">
+      <header>
+        <img src={selectedAnime.image} alt={`${selectedAnime.title} cover`} />
+        <div className="details-overview">
+          <h2>{selectedAnime.title}</h2>
+          <p>
+            {selectedAnime.year} &bull; {selectedAnime.score}
+          </p>
+        </div>
+      </header>
+      <section>
+        <p>
+          <em>{selectedAnime.synopsis}</em>
+        </p>
+      </section>
+    </div>
   );
 }
